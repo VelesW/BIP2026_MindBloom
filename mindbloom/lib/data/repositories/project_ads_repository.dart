ef8ccbd2a9cd7a81a616766596dfc2d6e33a02e7
VelesW <michalwilk296@gmail.com>
2026-01-13 09:58:45 +0100

    App init setup

---
+++ mindbloom/lib/data/repositories/project_ads_repository.dart
@@ -0,0 +1,11 @@
+import 'package:mindbloom/data/datasource/project_ads_remote_ds.dart';
+
+import '../models/project_ad.dart';
+
+class ProjectAdsRepository {
+  final ProjectAdsRemoteDataSource remote;
+
+  ProjectAdsRepository(this.remote);
+
+  Future<void> createAd(ProjectAd ad) => remote.createAd(ad);
+}
